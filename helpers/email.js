7e510d92491388c7187df1e177b0e63a145740dc
silc22 <you@example.com>
2022-07-25 13:46:22 +0200

    sendgrid fixed

--- helpers/email.js
+++ helpers/email.js
@@ -1,61 +1,55 @@
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
 
-const registrationEmail = async (data) =>{
+
+const registrationEmail = (data) =>{
   const { email, name, token } = data
   
-  const transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: 'silcerquatti@gmail.com', // Use the email address or domain you verified above
+    subject: "MERN project - Check your account",
+    text: "Check your account in this MERN project",
+    html:`
+    <p> 
+      ¡Hi ${name}!
+    </p>
+    <p> 
+      Your account is almost ready, to check it please click 
+      <a href="${process.env.FRONTEND_URL}/confirm/${token}">
+        HERE
+      </a> 
+      and you will be able to create your own projects!
+    </p>
+    <p>
+      If you did not create this account, ignore this message
+    </p>
+  `,
+  };
+
+
+  //ES6
+  sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+    
+    if (error.response) {
+      console.error(error.response.body)
     }
   });
-
-  //email info
-  const info = await transport.sendMail({
-    from: '"MERN project" <silcerquatti@gmail.com>', 
-    to: email,
-    subjecto: "MERN project - Check your account",
-    text: "Check your account in this MERN project",
-    html: `
-      <p> 
-        ¡Hi ${name}!
-      </p>
-      <p> 
-        Your account is almost ready, to check it please click 
-        <a href="${process.env.FRONTEND_URL}/confirm/${token}">
-          HERE
-        </a> 
-        and you will be able to create your own projects!
-      </p>
-      <p>
-        If you did not create this account, ignore this message
-      </p>
-    `,
-  })
-
   }
+
 
   const emailForgotPassword = async (data) =>{
     const { email, name, token } = data
-    
-    //TODO: HACER VARIABLE DE ENTORNO
-    const transport = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
   
-    //email info
-    const info = await transport.sendMail({
-      from: '"MERN project" <silcerquatti@gmail.com>', 
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
       to: email,
-      subjecto: "MERN project - Reset your password",
-      text: "Reset your password",
+      from: 'silcerquatti@gmail.com', // Use the email address or domain you verified above
+      subject: "MERN project - Check your account",
+      text: "Check your account in this MERN project",
       html: `
         <p> 
           ¡Hi ${name}!
@@ -71,8 +65,18 @@ const registrationEmail = async (data) =>{
           If you did not create this account, ignore this message.
         </p>
       `,
-    })
-  
     }
+
+  sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+    
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+  
+  }
   
 module.exports = {registrationEmail, emailForgotPassword}
